8ccb5b632fcdbe7d7a394add1162b1d2990c3f24
Riyan Ali <riyanaliofficial@gmail.com>
2025-08-15 17:16:59 +0530

    testimonials refetching issue resolved

--- components/testimonials-slider.tsx
+++ components/testimonials-slider.tsx
@@ -33,7 +33,7 @@ export function TestimonialsSlider({ initialTestimonials = [] }: TestimonialsSli
     if (initialTestimonials.length === 0) {
       fetchTestimonials();
     }
-  }, [initialTestimonials]);
+  }, []);
 
   useEffect(() => {
     if (testimonials.length > 0) {
